4393ed73117e76268a0c7601406e62427b286473
sannanhafeez <sannanhafeez06@gmail.com>
2019-07-06 14:10:36 +1200

    Added topbar component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,6 +6,12 @@ export default class MainPage extends Component {
         return (
             <React.Fragment>
                 <Topbar />
+                <div className="container-fluid" style={{height: '100vh'}}>
+                    <div className="row">
+                        <div className="col-6"></div>
+                        <div className="col-6"></div>
+                    </div>
+                </div>
             </React.Fragment>
         )
     }
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import justtext from '../images/justtext.png';
-
+import Button from '@material-ui/core/Button';
 
 export default class TopBar extends Component {
     render() {
@@ -9,6 +9,11 @@ export default class TopBar extends Component {
                 <div className="row">
                     <div className="col-6">
                         <img src={justtext} className="topbar-logo" alt="Planit"/>
+                    </div>
+                    <div className="col-6">
+                        <Button style={{color:'white', position:'absolute', right:10, top:10, outline:'none'}}>
+                            Log In
+                        </Button>
                     </div>
                 </div>
             </div>
